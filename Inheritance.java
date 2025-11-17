84256893e9e79e3003c701f115ca9057346cfb8a
Lee Javier <leejavier753@gmail.com>
2025-11-17 15:15:09 +0800

    Create Inheritance.java

---
+++ Inheritance.java
@@ -0,0 +1,53 @@
+class Vehicle {
+    void info() {
+        System.out.println("This is a vehicle");
+    }
+
+    void info(String brand) {
+        System.out.println("Brand: " + brand);
+    }
+
+    void info(String brand, int year, String color, String transmission, String gas) {
+        System.out.println("Brand: " + brand + ", Year: " + year + ", Color: " + color + ", Transmission: " + transmission + ", Gas: " + gas);
+    }
+
+    // Method to be overridden
+    void speed() {
+        System.out.println("Vehicle has an average speed.");
+    }
+}
+
+class Car extends Vehicle {
+    @Override
+    void speed() {
+        System.out.println("Car runs at 150 km/h.");
+    }
+}
+
+class Motorcycle extends Vehicle {
+    @Override
+    void speed() {
+        System.out.println("Motorcycle runs at 120 km/h.");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Vehicle v;
+      
+        v = new Car();
+        v.info();
+        v.info("Toyota");
+        v.info("Toyota", 2024, "Red", "Automatic", "Unleaded");
+        v.speed();
+
+        System.out.println();
+
+        v = new Motorcycle();
+        v.info();
+        v.info("Honda");
+        v.info("Honda", 2020, "Red", "Automatic", "Unleaded");
+        v.speed();
+    }
+}
