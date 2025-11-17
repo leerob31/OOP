8658b81d4dde5ae79b63a8c7fc6994d2509c9934
Lee Javier <leejavier753@gmail.com>
2025-11-17 20:10:51 +0800

    Create Encap.java

---
+++ Encap.java
@@ -0,0 +1,32 @@
+
+class BankAccount {
+
+    private final String owner;
+    private double balance;
+
+    public BankAccount(String owner, double balance) {
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+        System.out.println(owner + " deposited Php " + amount + ". New balance: Php " + balance);
+    }
+
+    public void withdraw(double amount) {
+        if (amount <= balance) {
+            balance -= amount;
+            System.out.println(owner + " withdrew Php " + amount + ". Remaining balance: Php " + balance);
+        } else {
+            System.out.println("Insufficient funds!");
+        }
+    }
+
+    public static void main(String[] args) {
+        BankAccount amount1 = new BankAccount("Alvin", 100000);
+        amount1.deposit(1500);
+        amount1.withdraw(2000);
+        amount1.withdraw(100001);
+    }
+}
