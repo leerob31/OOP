ac5438e66a37c520961890d0394d927967bb3817
Lee Javier <leejavier753@gmail.com>
2025-11-17 15:17:12 +0800

    Create Abstract.java

---
+++ Abstract.java
@@ -0,0 +1,39 @@
+abstract class Car {
+    String brand = "Toyota";
+
+    abstract void startEngine();
+    abstract void stopEngine();
+
+    void showBrand() {
+        System.out.println("Car Brand: " + brand);
+    }
+}
+
+class ToyotaVios extends Car {
+
+    @Override
+    void startEngine() {
+        System.out.println("Toyota Vios engine started using push button.");
+    }
+
+    @Override
+    void stopEngine() {
+        System.out.println("Toyota Vios engine stopped. When you step on a brake the brake caliper will pressed the disc brake until it stops");
+    }
+
+    void showModel() {
+        System.out.println("Model: Toyota Vios 2024");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new ToyotaVios(); 
+
+        myCar.showBrand();
+        myCar.startEngine();
+        myCar.stopEngine();
+
+        ((ToyotaVios) myCar).showModel();
+    }
+}
